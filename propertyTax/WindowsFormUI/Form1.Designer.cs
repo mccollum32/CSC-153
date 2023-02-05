165d6aa1ef57558ce03fd8c56e39d0c93716438e
Joseph McCollum <103016879+mccollum32@users.noreply.github.com>
2023-02-05 13:31:32 -0500

    Add files via upload

---
+++ propertyTax/WindowsFormUI/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿
+namespace WindowsFormUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.enterBtn = new System.Windows.Forms.Button();
+            this.clearBtn = new System.Windows.Forms.Button();
+            this.exitBtn = new System.Windows.Forms.Button();
+            this.propertyLbl = new System.Windows.Forms.TextBox();
+            this.taxLbl = new System.Windows.Forms.TextBox();
+            this.totalLbl = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // enterBtn
+            // 
+            this.enterBtn.Location = new System.Drawing.Point(79, 368);
+            this.enterBtn.Name = "enterBtn";
+            this.enterBtn.Size = new System.Drawing.Size(171, 38);
+            this.enterBtn.TabIndex = 0;
+            this.enterBtn.Text = "Enter";
+            this.enterBtn.UseVisualStyleBackColor = true;
+            this.enterBtn.Click += new System.EventHandler(this.enterBtn_Click);
+            // 
+            // clearBtn
+            // 
+            this.clearBtn.Location = new System.Drawing.Point(298, 368);
+            this.clearBtn.Name = "clearBtn";
+            this.clearBtn.Size = new System.Drawing.Size(166, 38);
+            this.clearBtn.TabIndex = 1;
+            this.clearBtn.Text = "Clear";
+            this.clearBtn.UseVisualStyleBackColor = true;
+            this.clearBtn.Click += new System.EventHandler(this.clearBtn_Click);
+            // 
+            // exitBtn
+            // 
+            this.exitBtn.Location = new System.Drawing.Point(506, 368);
+            this.exitBtn.Name = "exitBtn";
+            this.exitBtn.Size = new System.Drawing.Size(145, 38);
+            this.exitBtn.TabIndex = 2;
+            this.exitBtn.Text = "Exit";
+            this.exitBtn.UseVisualStyleBackColor = true;
+            this.exitBtn.Click += new System.EventHandler(this.exitBtn_Click);
+            // 
+            // propertyLbl
+            // 
+            this.propertyLbl.Location = new System.Drawing.Point(79, 199);
+            this.propertyLbl.Name = "propertyLbl";
+            this.propertyLbl.Size = new System.Drawing.Size(171, 26);
+            this.propertyLbl.TabIndex = 3;
+            // 
+            // taxLbl
+            // 
+            this.taxLbl.Location = new System.Drawing.Point(298, 198);
+            this.taxLbl.Name = "taxLbl";
+            this.taxLbl.ReadOnly = true;
+            this.taxLbl.Size = new System.Drawing.Size(166, 26);
+            this.taxLbl.TabIndex = 4;
+            // 
+            // totalLbl
+            // 
+            this.totalLbl.Location = new System.Drawing.Point(506, 198);
+            this.totalLbl.Name = "totalLbl";
+            this.totalLbl.ReadOnly = true;
+            this.totalLbl.Size = new System.Drawing.Size(145, 26);
+            this.totalLbl.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(106, 249);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Property Value";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(324, 249);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Property Tax";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(553, 249);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Total";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(721, 518);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.totalLbl);
+            this.Controls.Add(this.taxLbl);
+            this.Controls.Add(this.propertyLbl);
+            this.Controls.Add(this.exitBtn);
+            this.Controls.Add(this.clearBtn);
+            this.Controls.Add(this.enterBtn);
+            this.Name = "Form1";
+            this.Text = "Property Tax";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button enterBtn;
+        private System.Windows.Forms.Button clearBtn;
+        private System.Windows.Forms.Button exitBtn;
+        private System.Windows.Forms.TextBox propertyLbl;
+        private System.Windows.Forms.TextBox taxLbl;
+        private System.Windows.Forms.TextBox totalLbl;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ propertyTax/WindowsFormUI/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+/**
+ * Feb 5 23
+ * CSC 153
+ * McCollum Joseph
+ * This app takes user input tax and converts tax and the price output
+ */
+
+namespace WindowsFormUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void exitBtn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void clearBtn_Click(object sender, EventArgs e)
+        {
+            propertyLbl.Text = "";
+            taxLbl.Text = "";
+            totalLbl.Text = "";
+        }
+
+        private void enterBtn_Click(object sender, EventArgs e)
+        {
+            decimal property;
+            decimal total;
+            decimal taxed;
+
+            property = decimal.Parse(propertyLbl.Text);
+            taxed = (property / 100) * 0.64m;
+            total = property + taxed;
+            
+            taxLbl.Text = taxed.ToString("c");
+            totalLbl.Text = total.ToString("c");
+        }
+    }
+}
