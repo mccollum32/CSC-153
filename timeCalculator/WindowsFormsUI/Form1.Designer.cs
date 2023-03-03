cb091ac1631439dcbb356ceae0613afef1b6021c
Joseph McCollum <103016879+mccollum32@users.noreply.github.com>
2023-03-02 20:02:04 -0500

    Add files via upload

---
+++ timeCalculator/WindowsFormsUI/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿
+namespace WindowsFormsUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.timeBox = new System.Windows.Forms.TextBox();
+            this.enterBtn = new System.Windows.Forms.Button();
+            this.clearBtn = new System.Windows.Forms.Button();
+            this.exitBtn = new System.Windows.Forms.Button();
+            this.resultBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(252, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter Seconds";
+            // 
+            // timeBox
+            // 
+            this.timeBox.Location = new System.Drawing.Point(373, 70);
+            this.timeBox.Name = "timeBox";
+            this.timeBox.Size = new System.Drawing.Size(145, 26);
+            this.timeBox.TabIndex = 1;
+            // 
+            // enterBtn
+            // 
+            this.enterBtn.Location = new System.Drawing.Point(256, 257);
+            this.enterBtn.Name = "enterBtn";
+            this.enterBtn.Size = new System.Drawing.Size(111, 40);
+            this.enterBtn.TabIndex = 2;
+            this.enterBtn.Text = "Enter";
+            this.enterBtn.UseVisualStyleBackColor = true;
+            this.enterBtn.Click += new System.EventHandler(this.enterBtn_Click);
+            // 
+            // clearBtn
+            // 
+            this.clearBtn.Location = new System.Drawing.Point(407, 257);
+            this.clearBtn.Name = "clearBtn";
+            this.clearBtn.Size = new System.Drawing.Size(111, 40);
+            this.clearBtn.TabIndex = 3;
+            this.clearBtn.Text = "Clear";
+            this.clearBtn.UseVisualStyleBackColor = true;
+            this.clearBtn.Click += new System.EventHandler(this.clearBtn_Click);
+            // 
+            // exitBtn
+            // 
+            this.exitBtn.Location = new System.Drawing.Point(256, 331);
+            this.exitBtn.Name = "exitBtn";
+            this.exitBtn.Size = new System.Drawing.Size(262, 39);
+            this.exitBtn.TabIndex = 4;
+            this.exitBtn.Text = "Exit";
+            this.exitBtn.UseVisualStyleBackColor = true;
+            this.exitBtn.Click += new System.EventHandler(this.exitBtn_Click);
+            // 
+            // resultBox
+            // 
+            this.resultBox.Location = new System.Drawing.Point(203, 166);
+            this.resultBox.Name = "resultBox";
+            this.resultBox.ReadOnly = true;
+            this.resultBox.Size = new System.Drawing.Size(369, 26);
+            this.resultBox.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.resultBox);
+            this.Controls.Add(this.exitBtn);
+            this.Controls.Add(this.clearBtn);
+            this.Controls.Add(this.enterBtn);
+            this.Controls.Add(this.timeBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox timeBox;
+        private System.Windows.Forms.Button enterBtn;
+        private System.Windows.Forms.Button clearBtn;
+        private System.Windows.Forms.Button exitBtn;
+        private System.Windows.Forms.TextBox resultBox;
+    }
+}
+
---
+++ timeCalculator/WindowsFormsUI/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/**
+* Feb 28 23
+* CSC 153
+* McCollum Joseph
+* This program takes a user input and converts seconds, hours, or days and shows what it is equal to  
+*/
+
+namespace WindowsFormsUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void clearBtn_Click(object sender, EventArgs e)
+        {
+            timeBox.Text = "";
+            resultBox.Text = "";
+            timeBox.Focus();
+        }
+
+        private void exitBtn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void enterBtn_Click(object sender, EventArgs e)
+        {
+            int timeSec;
+
+            if (int.TryParse(timeBox.Text, out timeSec) && timeSec >= 60)
+            {
+                const double secondMin = 60.0;
+                const double hourMin = 3600.0;
+                const double dayMin = 86400.0;
+
+                if (timeSec >= secondMin && timeSec < hourMin)
+                {
+                    double clockMin = timeSec / secondMin;
+                    resultBox.Text = (timeSec + " seconds is the same as " + clockMin.ToString("n2") + " minutes");
+
+                }
+                else if (timeSec >= hourMin && timeSec < dayMin)
+                {
+                    double clockHour = timeSec / hourMin;
+                    resultBox.Text = (timeSec + " seconds is the same as " + clockHour.ToString("n2") + " hours");
+
+                }
+                else if (timeSec >= dayMin)
+                {
+                    double clockDay = timeSec / dayMin;
+                    resultBox.Text = (timeSec + " seconds is the same as " + clockDay.ToString("n2") + " days");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter a number not a string and greater than or equal to 60!");
+            }
+        }
+               
+    }
+}
