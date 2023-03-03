ebd0c858713a54e039442dfc382fc01dede598b7
Joseph McCollum <103016879+mccollum32@users.noreply.github.com>
2023-03-02 20:02:56 -0500

    Add files via upload

---
+++ changeForDollar/WindowsFormUI/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿
+namespace WindowsFormUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pennyBox = new System.Windows.Forms.TextBox();
+            this.nickelBox = new System.Windows.Forms.TextBox();
+            this.dimeBox = new System.Windows.Forms.TextBox();
+            this.qtrBox = new System.Windows.Forms.TextBox();
+            this.enterBtn = new System.Windows.Forms.Button();
+            this.clearBtn = new System.Windows.Forms.Button();
+            this.exitBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(122, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Penny Amount:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(122, 135);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(115, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nickel Amount:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(122, 198);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(110, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Dime Amount:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(122, 256);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(122, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Quater Amount:";
+            // 
+            // pennyBox
+            // 
+            this.pennyBox.Location = new System.Drawing.Point(288, 68);
+            this.pennyBox.Name = "pennyBox";
+            this.pennyBox.Size = new System.Drawing.Size(194, 26);
+            this.pennyBox.TabIndex = 4;
+            // 
+            // nickelBox
+            // 
+            this.nickelBox.Location = new System.Drawing.Point(288, 129);
+            this.nickelBox.Name = "nickelBox";
+            this.nickelBox.Size = new System.Drawing.Size(194, 26);
+            this.nickelBox.TabIndex = 5;
+            // 
+            // dimeBox
+            // 
+            this.dimeBox.Location = new System.Drawing.Point(288, 192);
+            this.dimeBox.Name = "dimeBox";
+            this.dimeBox.Size = new System.Drawing.Size(194, 26);
+            this.dimeBox.TabIndex = 6;
+            // 
+            // qtrBox
+            // 
+            this.qtrBox.Location = new System.Drawing.Point(288, 250);
+            this.qtrBox.Name = "qtrBox";
+            this.qtrBox.Size = new System.Drawing.Size(194, 26);
+            this.qtrBox.TabIndex = 7;
+            // 
+            // enterBtn
+            // 
+            this.enterBtn.Location = new System.Drawing.Point(552, 68);
+            this.enterBtn.Name = "enterBtn";
+            this.enterBtn.Size = new System.Drawing.Size(206, 87);
+            this.enterBtn.TabIndex = 8;
+            this.enterBtn.Text = "Enter";
+            this.enterBtn.UseVisualStyleBackColor = true;
+            this.enterBtn.Click += new System.EventHandler(this.enterBtn_Click);
+            // 
+            // clearBtn
+            // 
+            this.clearBtn.Location = new System.Drawing.Point(552, 192);
+            this.clearBtn.Name = "clearBtn";
+            this.clearBtn.Size = new System.Drawing.Size(206, 84);
+            this.clearBtn.TabIndex = 9;
+            this.clearBtn.Text = "Clear";
+            this.clearBtn.UseVisualStyleBackColor = true;
+            this.clearBtn.Click += new System.EventHandler(this.clearBtn_Click);
+            // 
+            // exitBtn
+            // 
+            this.exitBtn.Location = new System.Drawing.Point(552, 321);
+            this.exitBtn.Name = "exitBtn";
+            this.exitBtn.Size = new System.Drawing.Size(206, 81);
+            this.exitBtn.TabIndex = 10;
+            this.exitBtn.Text = "Exit";
+            this.exitBtn.UseVisualStyleBackColor = true;
+            this.exitBtn.Click += new System.EventHandler(this.exitBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.exitBtn);
+            this.Controls.Add(this.clearBtn);
+            this.Controls.Add(this.enterBtn);
+            this.Controls.Add(this.qtrBox);
+            this.Controls.Add(this.dimeBox);
+            this.Controls.Add(this.nickelBox);
+            this.Controls.Add(this.pennyBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox pennyBox;
+        private System.Windows.Forms.TextBox nickelBox;
+        private System.Windows.Forms.TextBox dimeBox;
+        private System.Windows.Forms.TextBox qtrBox;
+        private System.Windows.Forms.Button enterBtn;
+        private System.Windows.Forms.Button clearBtn;
+        private System.Windows.Forms.Button exitBtn;
+    }
+}
+
---
+++ changeForDollar/WindowsFormUI/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/**
+* Feb 17 23
+* CSC 153
+* McCollum Joseph
+* The purpose of this program is to take intput from a user 
+* and convert it into currency format and equal up to a $1
+* using IF/Else format
+*/
+
+
+
+namespace WindowsFormUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //The clear button to clear the output and focus on the penny box
+        private void clearBtn_Click(object sender, EventArgs e)
+        {
+            pennyBox.Text = "";
+            nickelBox.Text = "";
+            dimeBox.Text = "";
+            qtrBox.Text = "";
+            pennyBox.Focus();
+        }
+
+        private void exitBtn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void enterBtn_Click(object sender, EventArgs e)
+        {
+            //Integer input for the text box to convert the Int output
+            int pennies, nickels, dimes, qtrs;
+            pennies = Convert.ToInt32(pennyBox.Text);
+            nickels = Convert.ToInt32(nickelBox.Text);
+            dimes = Convert.ToInt32(dimeBox.Text);
+            qtrs = Convert.ToInt32(qtrBox.Text);
+            
+            //Calculate the total coins inputted by the user
+            const double penny = 0.01;
+            const double nickel = 0.05;
+            const double dime = 0.10;
+            const double quater = 0.25;
+            const double dollar = 1.00;
+
+            double pennyTotal = pennies * penny;
+            double nickelsTotal = nickels * nickel;
+            double dimeTotal = dimes * dime;
+            double quaterTotal = qtrs * quater;
+            double totalChange = pennyTotal + nickelsTotal + dimeTotal + quaterTotal;
+
+            //IF/Else statement using ToString format to format the currency output
+            if(totalChange == dollar)
+            {
+                MessageBox.Show("You Win! You have entered " + totalChange.ToString("c"));              
+            }
+            else if (totalChange <= dollar)
+            {
+                MessageBox.Show("Incorrect Amount! Your amount " + totalChange.ToString("c") + "is less!");
+            }
+            else if (totalChange >= dollar)
+            {
+                MessageBox.Show("Incorrect Amount! Your amount " + totalChange.ToString("c") + " is more!");
+            }
+            else
+            {
+                MessageBox.Show("Invalid entry try again!");
+            }
+     
+
+        }
+    }
+}
