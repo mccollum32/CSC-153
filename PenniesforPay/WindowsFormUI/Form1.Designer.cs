d5849e7b50a5b4f4b938c14791dab1667672aa86
Joseph McCollum <103016879+mccollum32@users.noreply.github.com>
2023-03-02 20:01:27 -0500

    Add files via upload

---
+++ PenniesforPay/WindowsFormUI/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿
+namespace WindowsFormUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.calcBtn = new System.Windows.Forms.Button();
+            this.clearBtn = new System.Windows.Forms.Button();
+            this.exitBtn = new System.Windows.Forms.Button();
+            this.dayBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listResults = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // calcBtn
+            // 
+            this.calcBtn.Location = new System.Drawing.Point(181, 501);
+            this.calcBtn.Name = "calcBtn";
+            this.calcBtn.Size = new System.Drawing.Size(144, 38);
+            this.calcBtn.TabIndex = 0;
+            this.calcBtn.Text = "Calculate";
+            this.calcBtn.UseVisualStyleBackColor = true;
+            this.calcBtn.Click += new System.EventHandler(this.calcBtn_Click);
+            // 
+            // clearBtn
+            // 
+            this.clearBtn.Location = new System.Drawing.Point(331, 501);
+            this.clearBtn.Name = "clearBtn";
+            this.clearBtn.Size = new System.Drawing.Size(144, 38);
+            this.clearBtn.TabIndex = 1;
+            this.clearBtn.Text = "Clear";
+            this.clearBtn.UseVisualStyleBackColor = true;
+            this.clearBtn.Click += new System.EventHandler(this.clearBtn_Click);
+            // 
+            // exitBtn
+            // 
+            this.exitBtn.Location = new System.Drawing.Point(481, 502);
+            this.exitBtn.Name = "exitBtn";
+            this.exitBtn.Size = new System.Drawing.Size(144, 37);
+            this.exitBtn.TabIndex = 2;
+            this.exitBtn.Text = "Exit";
+            this.exitBtn.UseVisualStyleBackColor = true;
+            this.exitBtn.Click += new System.EventHandler(this.exitBtn_Click);
+            // 
+            // dayBox
+            // 
+            this.dayBox.Location = new System.Drawing.Point(352, 48);
+            this.dayBox.Name = "dayBox";
+            this.dayBox.Size = new System.Drawing.Size(144, 26);
+            this.dayBox.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(112, 271);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Results";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(177, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(169, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Enter Number Of Days";
+            // 
+            // listResults
+            // 
+            this.listResults.FormattingEnabled = true;
+            this.listResults.ItemHeight = 20;
+            this.listResults.Location = new System.Drawing.Point(181, 105);
+            this.listResults.Name = "listResults";
+            this.listResults.Size = new System.Drawing.Size(444, 364);
+            this.listResults.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 660);
+            this.Controls.Add(this.listResults);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dayBox);
+            this.Controls.Add(this.exitBtn);
+            this.Controls.Add(this.clearBtn);
+            this.Controls.Add(this.calcBtn);
+            this.Name = "Form1";
+            this.Text = "Pennies For Pay";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button calcBtn;
+        private System.Windows.Forms.Button clearBtn;
+        private System.Windows.Forms.Button exitBtn;
+        private System.Windows.Forms.TextBox dayBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listResults;
+    }
+}
+
---
+++ PenniesforPay/WindowsFormUI/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+/**
+* 3 Mar 23
+* CSC 153
+* McCollum Joseph
+* This program uses a For Loop to calculate the number of days after a user has inputted the number of days
+* for each number of days the penny amount increases by 2
+*/
+
+
+
+namespace WindowsFormUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void clearBtn_Click(object sender, EventArgs e)
+        {
+            dayBox.Text = "";
+            listResults.Items.Clear();
+            dayBox.Focus();
+        }
+
+        private void exitBtn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void calcBtn_Click(object sender, EventArgs e)
+        {
+            int payDays;
+
+            const double PENNIES = 0.01;
+            
+
+            if (int.TryParse(dayBox.Text, out payDays) && payDays >= 1)
+            {
+                double pay = PENNIES;
+
+                for (int day = 1; day <= payDays; day++)
+                {
+                    listResults.Items.Add("Pay after day " + day + "," + " Susan has made: " + pay.ToString("c"));
+                    pay += pay;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Invalid input try again.");
+
+            }
+        }
+    }
+}
