3f6f78fea693b7e3a5f5fad37b96599f93b93404
Joseph McCollum <103016879+mccollum32@users.noreply.github.com>
2023-03-12 17:17:13 -0400

    Add files via upload

---
+++ randNumFileReader/WindowsFormUI/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿
+namespace WindowsFormUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.readBtn = new System.Windows.Forms.Button();
+            this.exitBtn = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // readBtn
+            // 
+            this.readBtn.Location = new System.Drawing.Point(81, 423);
+            this.readBtn.Name = "readBtn";
+            this.readBtn.Size = new System.Drawing.Size(181, 62);
+            this.readBtn.TabIndex = 0;
+            this.readBtn.Text = "Read";
+            this.readBtn.UseVisualStyleBackColor = true;
+            this.readBtn.Click += new System.EventHandler(this.readBtn_Click);
+            // 
+            // exitBtn
+            // 
+            this.exitBtn.Location = new System.Drawing.Point(363, 423);
+            this.exitBtn.Name = "exitBtn";
+            this.exitBtn.Size = new System.Drawing.Size(181, 62);
+            this.exitBtn.TabIndex = 1;
+            this.exitBtn.Text = "Exit";
+            this.exitBtn.UseVisualStyleBackColor = true;
+            this.exitBtn.Click += new System.EventHandler(this.exitBtn_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(81, 67);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(463, 304);
+            this.listBox1.TabIndex = 4;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(633, 580);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.exitBtn);
+            this.Controls.Add(this.readBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button readBtn;
+        private System.Windows.Forms.Button exitBtn;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ randNumFileReader/WindowsFormUI/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+/**
+* Mar 12 23
+* CSC 153
+* McCollum Joseph
+* This program reads the files from "Random File Writer" and displays the information in the list box
+*/
+
+namespace WindowsFormUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void exitBtn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void readBtn_Click(object sender, EventArgs e)
+        {
+            StreamReader inputFile;
+
+            try
+            {
+                int num;
+                int total = 0;
+                int count = 0;
+
+                if(openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    inputFile = File.OpenText(openFileDialog1.FileName);
+
+                    while (!inputFile.EndOfStream)
+                    {
+                        num = int.Parse(inputFile.ReadLine());
+                        listBox1.Items.Add(num);
+                        total += num;
+                        count++;
+                    }
+                    listBox1.Items.Add("Total: " + total);
+                    listBox1.Items.Add("Random numbers read: " + count);
+                    MessageBox.Show("Success!\nNumbers from: \n" + openFileDialog1.FileName + "\nhave been read!");
+                }
+                else
+                {
+                    MessageBox.Show("Operation Terminated!");
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
