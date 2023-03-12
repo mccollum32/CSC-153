23528deaed972ce96dfdc00a8d3762a4de030444
Joseph McCollum <103016879+mccollum32@users.noreply.github.com>
2023-03-12 15:37:03 -0400

    Add files via upload

---
+++ randomFileWriter/WindowsFormUI/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿
+namespace WindowsFormUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.saveFilebtn = new System.Windows.Forms.Button();
+            this.exitBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.userNum = new System.Windows.Forms.TextBox();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // saveFilebtn
+            // 
+            this.saveFilebtn.Location = new System.Drawing.Point(65, 211);
+            this.saveFilebtn.Name = "saveFilebtn";
+            this.saveFilebtn.Size = new System.Drawing.Size(161, 59);
+            this.saveFilebtn.TabIndex = 2;
+            this.saveFilebtn.Text = "Save";
+            this.saveFilebtn.UseVisualStyleBackColor = true;
+            this.saveFilebtn.Click += new System.EventHandler(this.saveFilebtn_Click);
+            // 
+            // exitBtn
+            // 
+            this.exitBtn.Location = new System.Drawing.Point(289, 211);
+            this.exitBtn.Name = "exitBtn";
+            this.exitBtn.Size = new System.Drawing.Size(160, 59);
+            this.exitBtn.TabIndex = 3;
+            this.exitBtn.Text = "Exit";
+            this.exitBtn.UseVisualStyleBackColor = true;
+            this.exitBtn.Click += new System.EventHandler(this.exitBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(61, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Enter Number:";
+            // 
+            // userNum
+            // 
+            this.userNum.Location = new System.Drawing.Point(179, 83);
+            this.userNum.Name = "userNum";
+            this.userNum.Size = new System.Drawing.Size(270, 26);
+            this.userNum.TabIndex = 5;
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "Text files|*.txt";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(518, 330);
+            this.Controls.Add(this.userNum);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.exitBtn);
+            this.Controls.Add(this.saveFilebtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button saveFilebtn;
+        private System.Windows.Forms.Button exitBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox userNum;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ randomFileWriter/WindowsFormUI/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+/**
+* Mar 12 23
+* CSC 153
+* McCollum Joseph
+* This program takes input from a user with an interger from 1 - 100 and writes the random number to a file
+*/
+
+namespace WindowsFormUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void exitBtn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void saveFilebtn_Click(object sender, EventArgs e)
+        {            
+            SaveFileDialog saveFile = new SaveFileDialog();            
+            saveFile.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            saveFile.FilterIndex = 2;
+            saveFile.RestoreDirectory = true;
+            StreamWriter outputFile;
+            Random rand = new Random();
+            int numbers;
+
+            if (int.TryParse(userNum.Text, out numbers) && numbers > 0)
+            {
+                if (saveFile.ShowDialog() == DialogResult.OK)
+                {
+                    outputFile = File.CreateText(saveFile.FileName);
+                    //Writes random numbers to the file using a for loop
+                    for (int count = 1; count <= numbers; count++)
+                    {
+                        int random1 = rand.Next(100) + 1;
+                        outputFile.WriteLine(random1);
+                    }
+                    outputFile.Close();
+                    MessageBox.Show(numbers + " Files have been written to \n " + saveFile.FileName);
+                    userNum.Clear();
+                    userNum.Focus();
+                }
+                else
+                {
+                    MessageBox.Show("Operation Cancelled!");
+                    userNum.Clear();
+                    userNum.Focus();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Invalid input!\nNumbers must be 1 - 100! \nTry again!");
+                userNum.Clear();
+                userNum.Focus();
+            }
+        }
+    }
+}
