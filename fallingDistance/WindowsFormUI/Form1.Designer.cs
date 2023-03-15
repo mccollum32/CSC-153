016e7387f8f442159cb3e0cab4cccf22a2ae9720
Joseph McCollum <103016879+mccollum32@users.noreply.github.com>
2023-03-15 02:53:48 -0400

    Add files via upload

---
+++ fallingDistance/WindowsFormUI/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿
+namespace WindowsFormUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.secBox = new System.Windows.Forms.TextBox();
+            this.totalBox = new System.Windows.Forms.TextBox();
+            this.calcBtn = new System.Windows.Forms.Button();
+            this.exitBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.clrBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // secBox
+            // 
+            this.secBox.Location = new System.Drawing.Point(247, 75);
+            this.secBox.Name = "secBox";
+            this.secBox.Size = new System.Drawing.Size(236, 26);
+            this.secBox.TabIndex = 0;
+            // 
+            // totalBox
+            // 
+            this.totalBox.Location = new System.Drawing.Point(247, 170);
+            this.totalBox.Name = "totalBox";
+            this.totalBox.ReadOnly = true;
+            this.totalBox.Size = new System.Drawing.Size(236, 26);
+            this.totalBox.TabIndex = 1;
+            // 
+            // calcBtn
+            // 
+            this.calcBtn.Location = new System.Drawing.Point(109, 265);
+            this.calcBtn.Name = "calcBtn";
+            this.calcBtn.Size = new System.Drawing.Size(127, 64);
+            this.calcBtn.TabIndex = 2;
+            this.calcBtn.Text = "Calculate";
+            this.calcBtn.UseVisualStyleBackColor = true;
+            this.calcBtn.Click += new System.EventHandler(this.calcBtn_Click);
+            // 
+            // exitBtn
+            // 
+            this.exitBtn.Location = new System.Drawing.Point(375, 265);
+            this.exitBtn.Name = "exitBtn";
+            this.exitBtn.Size = new System.Drawing.Size(127, 63);
+            this.exitBtn.TabIndex = 3;
+            this.exitBtn.Text = "Exit";
+            this.exitBtn.UseVisualStyleBackColor = true;
+            this.exitBtn.Click += new System.EventHandler(this.exitBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(117, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Enter Seconds:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(76, 176);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(165, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Total Falling Distance:";
+            // 
+            // clrBtn
+            // 
+            this.clrBtn.Location = new System.Drawing.Point(242, 266);
+            this.clrBtn.Name = "clrBtn";
+            this.clrBtn.Size = new System.Drawing.Size(127, 62);
+            this.clrBtn.TabIndex = 6;
+            this.clrBtn.Text = "Clear";
+            this.clrBtn.UseVisualStyleBackColor = true;
+            this.clrBtn.Click += new System.EventHandler(this.clrBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(570, 415);
+            this.Controls.Add(this.clrBtn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.exitBtn);
+            this.Controls.Add(this.calcBtn);
+            this.Controls.Add(this.totalBox);
+            this.Controls.Add(this.secBox);
+            this.Name = "Form1";
+            this.Text = "Fall Distance";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox secBox;
+        private System.Windows.Forms.TextBox totalBox;
+        private System.Windows.Forms.Button calcBtn;
+        private System.Windows.Forms.Button exitBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button clrBtn;
+    }
+}
+
---
+++ fallingDistance/WindowsFormUI/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+/**
+* Mar 15 23
+* CSC 153
+* Mccollum Joseph
+* This program accepts input from a user and calc seconds to the Falling Distance of a object. The method/functions calculate the distance formula
+* distance = 1/2 "0.5" * gt2 "Gravity * time(seconds) squared"
+*/
+
+namespace WindowsFormUI
+{
+    public partial class Form1 : Form
+    {
+        private const double GRAVITY = 9.8;
+        private const double DT = 0.5;
+
+        private double FallingDistance(double seconds)
+        {
+            double distance = (DT) * (GRAVITY) * (Math.Pow(seconds, 2));
+            return distance;
+        }
+
+        private bool IsInputValid(ref double timeSec)
+        {
+            bool valInput = false;
+
+            if (double.TryParse(secBox.Text, out timeSec) && timeSec >= 0)
+            {
+                timeSec = Math.Round(timeSec, 2);
+                secBox.Text = timeSec.ToString();
+                valInput = true;
+            }
+            else
+            {
+                MessageBox.Show("Invalid input! Try again.");
+                secBox.Clear();
+                secBox.Focus();
+            }
+
+            return valInput;
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void exitBtn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void calcBtn_Click(object sender, EventArgs e)
+        {
+            double timeSec = 0, distance;
+            if(IsInputValid(ref timeSec))
+            {
+                distance = Math.Round(FallingDistance(timeSec), 2);
+
+                totalBox.Text = distance.ToString("n2");
+
+                secBox.Focus();
+            }
+        }
+
+        private void clrBtn_Click(object sender, EventArgs e)
+        {
+            secBox.Clear();
+            secBox.Focus();
+            totalBox.Clear();
+        }
+    }
+}
