a9429fa30d9a5af3e1295fdedb9e818ccca0ae03
Joseph McCollum <103016879+mccollum32@users.noreply.github.com>
2023-03-21 02:05:13 -0400

    Add files via upload

---
+++ PrimeNumbers/WindowsFormUI/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿
+namespace WindowsFormUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.primeBox = new System.Windows.Forms.TextBox();
+            this.calcBtn = new System.Windows.Forms.Button();
+            this.exitBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // primeBox
+            // 
+            this.primeBox.Location = new System.Drawing.Point(245, 74);
+            this.primeBox.Name = "primeBox";
+            this.primeBox.Size = new System.Drawing.Size(226, 26);
+            this.primeBox.TabIndex = 0;
+            // 
+            // calcBtn
+            // 
+            this.calcBtn.Location = new System.Drawing.Point(12, 146);
+            this.calcBtn.Name = "calcBtn";
+            this.calcBtn.Size = new System.Drawing.Size(227, 70);
+            this.calcBtn.TabIndex = 1;
+            this.calcBtn.Text = "Calculate";
+            this.calcBtn.UseVisualStyleBackColor = true;
+            this.calcBtn.Click += new System.EventHandler(this.calcBtn_Click);
+            // 
+            // exitBtn
+            // 
+            this.exitBtn.Location = new System.Drawing.Point(245, 147);
+            this.exitBtn.Name = "exitBtn";
+            this.exitBtn.Size = new System.Drawing.Size(226, 69);
+            this.exitBtn.TabIndex = 2;
+            this.exitBtn.Text = "Exit";
+            this.exitBtn.UseVisualStyleBackColor = true;
+            this.exitBtn.Click += new System.EventHandler(this.exitBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(126, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Prime Number:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(516, 266);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.exitBtn);
+            this.Controls.Add(this.calcBtn);
+            this.Controls.Add(this.primeBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox primeBox;
+        private System.Windows.Forms.Button calcBtn;
+        private System.Windows.Forms.Button exitBtn;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ PrimeNumbers/WindowsFormUI/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+/**
+* Mar 20 23
+* CSC 153
+* Mccollum Joseph
+* This program uses a bool method/ for loop to calculate where the number a user entered is prime or not
+*/
+
+
+namespace WindowsFormUI
+{
+    public partial class Form1 : Form
+    {
+        private int numbers;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void exitBtn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private bool IsValid(ref int numbers)
+        {
+            bool valid = false;
+            if (int.TryParse(primeBox.Text, out numbers) && numbers >= 0)
+            {
+                valid = true;
+            }
+            else
+            {
+                MessageBox.Show("Enter a valid input! Try again");
+                primeBox.Focus();
+                primeBox.Clear();
+            }
+            return valid;
+        }
+        private bool Prime(int number)
+        {
+            for (int i = 2; i < number; i++)
+            {
+                if ((number % i) == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private void calcBtn_Click(object sender, EventArgs e)
+        {
+            if (IsValid(ref numbers))
+            {
+                if (Prime(numbers) && numbers != 0 && numbers != 1)
+                {
+                    MessageBox.Show("Number entered is prime!");
+                    primeBox.Clear();
+                    primeBox.Focus();
+                }
+                else
+                {
+                    MessageBox.Show("Number entered not prime. Try again!");
+                    primeBox.Clear();
+                    primeBox.Focus();
+                }
+            }
+        }
+    }
+}
