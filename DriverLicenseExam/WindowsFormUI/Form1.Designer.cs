58761375e78e0c31e5b6aea78ccc1812d10308c3
Joseph McCollum <103016879+mccollum32@users.noreply.github.com>
2023-05-03 16:11:43 -0400

    Add files via upload

---
+++ DriverLicenseExam/WindowsFormUI/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿
+namespace DriverLicenseExam
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.resultBtn = new System.Windows.Forms.Button();
+            this.clearBtn = new System.Windows.Forms.Button();
+            this.fileBox = new System.Windows.Forms.TextBox();
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.resultLBL = new System.Windows.Forms.TextBox();
+            this.exitBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "File Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 287);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Results:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 516);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(152, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Incorrect Questions:";
+            // 
+            // resultBtn
+            // 
+            this.resultBtn.Location = new System.Drawing.Point(170, 120);
+            this.resultBtn.Name = "resultBtn";
+            this.resultBtn.Size = new System.Drawing.Size(150, 80);
+            this.resultBtn.TabIndex = 3;
+            this.resultBtn.Text = "Results";
+            this.resultBtn.UseVisualStyleBackColor = true;
+            this.resultBtn.Click += new System.EventHandler(this.resultBtn_Click);
+            // 
+            // clearBtn
+            // 
+            this.clearBtn.Location = new System.Drawing.Point(461, 121);
+            this.clearBtn.Name = "clearBtn";
+            this.clearBtn.Size = new System.Drawing.Size(148, 79);
+            this.clearBtn.TabIndex = 4;
+            this.clearBtn.Text = "Clear";
+            this.clearBtn.UseVisualStyleBackColor = true;
+            this.clearBtn.Click += new System.EventHandler(this.clearBtn_Click);
+            // 
+            // fileBox
+            // 
+            this.fileBox.Location = new System.Drawing.Point(170, 51);
+            this.fileBox.Name = "fileBox";
+            this.fileBox.Size = new System.Drawing.Size(439, 26);
+            this.fileBox.TabIndex = 5;
+            // 
+            // listBox
+            // 
+            this.listBox.FormattingEnabled = true;
+            this.listBox.ItemHeight = 20;
+            this.listBox.Location = new System.Drawing.Point(170, 428);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(439, 204);
+            this.listBox.TabIndex = 7;
+            // 
+            // resultLBL
+            // 
+            this.resultLBL.Location = new System.Drawing.Point(170, 281);
+            this.resultLBL.Name = "resultLBL";
+            this.resultLBL.ReadOnly = true;
+            this.resultLBL.Size = new System.Drawing.Size(439, 26);
+            this.resultLBL.TabIndex = 9;
+            // 
+            // exitBtn
+            // 
+            this.exitBtn.Location = new System.Drawing.Point(326, 121);
+            this.exitBtn.Name = "exitBtn";
+            this.exitBtn.Size = new System.Drawing.Size(129, 79);
+            this.exitBtn.TabIndex = 10;
+            this.exitBtn.Text = "Exit";
+            this.exitBtn.UseVisualStyleBackColor = true;
+            this.exitBtn.Click += new System.EventHandler(this.exitBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(697, 676);
+            this.Controls.Add(this.exitBtn);
+            this.Controls.Add(this.resultLBL);
+            this.Controls.Add(this.listBox);
+            this.Controls.Add(this.fileBox);
+            this.Controls.Add(this.clearBtn);
+            this.Controls.Add(this.resultBtn);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button resultBtn;
+        private System.Windows.Forms.Button clearBtn;
+        private System.Windows.Forms.TextBox fileBox;
+        private System.Windows.Forms.ListBox listBox;
+        private System.Windows.Forms.TextBox resultLBL;
+        private System.Windows.Forms.Button exitBtn;
+    }
+}
+
